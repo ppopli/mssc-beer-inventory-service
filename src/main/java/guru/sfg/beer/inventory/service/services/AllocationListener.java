c70cf5cc14ab4a28149329c93d1f1d31e35404c4
Pulkit Popli <ppopli2793@gmail.com>
2023-06-24 16:27:52 +0530

    handle order allocation

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,32 @@
+package guru.sfg.beer.inventory.service.services;
+
+import com.pulkit.sfgBrewery.events.AllocateBeerOrderRequest;
+import com.pulkit.sfgBrewery.events.AllocateBeerOrderResult;
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AllocationListener {
+  private final AllocationService allocationService;
+  private final JmsTemplate jmsTemplate;
+
+  @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+  public void listen(AllocateBeerOrderRequest request) {
+    AllocateBeerOrderResult.AllocateBeerOrderResultBuilder builder = AllocateBeerOrderResult.builder();
+    builder.beerOrderDto(request.getBeerOrderDto());
+    try {
+      Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+      builder.pendingInventory(!allocationResult);
+    } catch (Exception e) {
+      log.error("error while allocating inventory");
+      builder.allocationError(true);
+    }
+    jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+  }
+}
---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,7 @@
+package guru.sfg.beer.inventory.service.services;
+
+import com.pulkit.sfgBrewery.model.BeerOrderDto;
+
+public interface AllocationService {
+  Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
